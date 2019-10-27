38edb69ec6c382b331fd486ad111bb7a95815d51
Arun Raj R <arunrajr.mvk@gmail.com>
2019-10-27 12:36:27 +0530

    Call JavaScript functions from Typescript.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//#region Methods which are defined in the .js files.
+
+declare function printResult(input: string): void;
+
+//#endregion
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'InvokeJsFuncFromTsAngular';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    printResult('Hello world!'); // Function call
+  }
+
 }
---
+++ src/assets/sample.js
@@ -0,0 +1,3 @@
+function printResult(input) {
+    alert(input);
+}
